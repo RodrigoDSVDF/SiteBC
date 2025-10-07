04158646ee7fbbdb94024e38925ca863ec2ba792
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-10-07 01:26:08 -0300

    Create tailwind.config.js

---
+++ sitebc/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'brand-background': '#E8DDD0',
+        'brand-beige': '#D4C4B0',
+        'brand-dark': '#4A3A28',
+        'brand-light': '#6A5A48',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    }
+  }
+}
